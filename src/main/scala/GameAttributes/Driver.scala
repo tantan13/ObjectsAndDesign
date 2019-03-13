4a3ce74f6576a9e2f54d732b6beb44847bfa82f6
Rahul Gautam <rgautam6@gatech.edu>
2019-03-13 00:07:35 -0400

    M2 Requirements Met

--- src/main/scala/GameAttributes/Driver.scala
+++ src/main/scala/GameAttributes/Driver.scala
@@ -11,6 +11,7 @@ object Driver extends App {
   var cont = true
   var numPlayers = 0
 
+  Console.println()
   while (cont) {
     Console.println("How many players do you have? ")
     numPlayers = keyboard.nextInt()
@@ -20,22 +21,59 @@ object Driver extends App {
       cont = false
     }
   }
-  val armiesDefault = 35
-  var turns = new ArrayList[Integer]();
+
+  val armiesDefault = numPlayers match {
+    case 3 => 35
+    case 4 => 30
+    case 5 => 25
+    case 6 => 20
+  }
+
+  var turns = new ArrayList[Integer]()
   for (i <- 1 to numPlayers) {
-    turns.add(i);
+    turns.add(i)
   }
   var players = new Array[Player](numPlayers)
 
+  Console.println()
   for (i <- 1 to numPlayers) {
     Console.println(s"Name for Player $i")
     val name = keyboard.next()
     var turn = turns.remove(random.nextInt(turns.size()))
-    players(i - 1) = new Player(name, turn, armiesDefault - (5 * (numPlayers - 3)))
+    players(i - 1) = new Player(name, turn, armiesDefault)
   }
 
-  for (player <- players) Console.println(player.toString)
+  Console.println("\nAssigned number of armies and Player Order:")
+  for (player <- players.sortBy(_.turnNo)) Console.println(player.toString)
 
+  Console.println("\nAvailable Continents and their territories")
+  Map.displayMap
 
+  Console.println("\nPlayers will now be assigned initial territories randomly")
+  Console.println("Press 'y' and enter to continue")
+  var muda = keyboard.next()
 
+  for (con <- Map.continents) {
+    for (terr <- con.territories) {
+      var rando = players(random.nextInt(numPlayers))
+      terr.owner = rando
+      terr.numArmies = 1
+      terr.owner.armies -= terr.numArmies
+    }
+  }
+
+  for (con <- Map.continents) {
+    for (terr <- con.territories) {
+      if (terr.owner.armies >= 10)
+        terr.numArmies += random.nextInt(10)
+      else if (terr.owner.armies < 10 && terr.owner.armies >= 1)
+        terr.numArmies += random.nextInt(terr.owner.armies)
+      else
+        terr.numArmies = terr.numArmies
+      terr.owner.armies -= terr.numArmies
+    }
+  }
+
+  Console.println("\nAssigned territories:")
+  Map.displayMap
 }
