a69cb8fe7485d9f7661501f5dde7b76bfc629cf0
Akshat Sistla <akshatsistla@lawn-128-61-125-191.lawn.gatech.edu>
2019-04-24 16:12:34 -0400

    Updated background

--- RiskGame/app/forms/PlayerForm.scala
+++ RiskGame/app/forms/PlayerForm.scala
@@ -8,12 +8,12 @@ case class PlayerForm(p1: String, p2: String, p3: String, p4: String, p5: String
 object PlayerForm {
   val playerForm: Form[PlayerForm] = Form(
     mapping(
-      "p1" -> nonEmptyText,
-      "p2" -> nonEmptyText,
-      "p3" -> nonEmptyText,
-      "p4" -> text,
-      "p5" -> text,
-      "p6" -> text
+      "Player 1" -> nonEmptyText,
+      "Player 2" -> nonEmptyText,
+      "Player 3" -> nonEmptyText,
+      "Player 4" -> text,
+      "Player 5" -> text,
+      "Player 6" -> text
     )(PlayerForm.apply)(PlayerForm.unapply)
   )
 }
